a0f79a0323b5fe85d7aa0059f4e818358ca4d462
翟长腿 <zhaiyuhanx@hotmail.com>
2020-03-29 19:08:34 +0800

    more support about the high dpi

--- MillenniumPlayer/main.cpp
+++ MillenniumPlayer/main.cpp
@@ -4,17 +4,18 @@
 
 int main(int argc, char *argv[])
 {
+	::SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 	QApplication a(argc, argv);
-	const float DEFAULT_DPI = 96.0;
+	/*const float DEFAULT_DPI = 96.0;
 	HDC screen = GetDC(NULL);
 	FLOAT dpiX = static_cast<FLOAT>(GetDeviceCaps(screen, LOGPIXELSX));
 	ReleaseDC(0, screen);
 	float fontSize = dpiX / DEFAULT_DPI;
-
 	QFont font = a.font();
 	font.setPointSize(font.pointSize() * fontSize);
-	a.setFont(font);
+	a.setFont(font);*/
 	MainView w;
 	w.show();
 	return a.exec();
